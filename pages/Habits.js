1ebfd3c1e48e8f65baf87dd4c2c34c2f422f22f8
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2022-11-12 15:34:39 +0530

    feat : render list of habits UI

--- pages/Habits.js
+++ pages/Habits.js
@@ -1,15 +1,47 @@
 import React from "react";
 import Image from "next/image";
-import {
-	Book,
-	BookOpen,
-	Calendar,
-	ChevronDown,
-	Wind,
-	Zap,
-} from "react-feather";
+import { BookOpen, Calendar, Check, Wind, Zap } from "react-feather";
 
 function Habits() {
+	const currDayHabits = [
+		{
+			name: "Workout",
+			isCompleted: false,
+			date: "12-11-2022",
+			color: "color1",
+		},
+		{
+			name: "Meditation",
+			isCompleted: false,
+			date: "12-11-2022",
+			color: "color2",
+		},
+		{
+			name: "Journal",
+			isCompleted: false,
+			date: "12-11-2022",
+			color: "color3",
+		},
+		{
+			name: "Reading",
+			isCompleted: false,
+			date: "12-11-2022",
+			color: "color4",
+		},
+		{
+			name: "Eat Muramba",
+			isCompleted: false,
+			date: "12-11-2022",
+			color: "color5",
+		},
+		{
+			name: "Drink Ashwaghandha Milk",
+			isCompleted: false,
+			date: "12-11-2022",
+			color: "color6",
+		},
+	];
+
 	return (
 		<div className="flex h-screen border">
 			<div className=" w-2/12  ">
@@ -40,10 +72,27 @@ function Habits() {
 				</ul>
 				<div className="h-[10%] flex items-center justify-center gap-2 border-t border-gray-200  ">
 					<div className=" w-12 h-12 bg-slate-300 rounded-full"></div>
-					<p className=" font-medium " >Saurabh</p>{" "}
+					<p className=" font-medium ">Saurabh</p>{" "}
 				</div>
 			</div>
-			<div className=" w-full bg-gray-50 "></div>
+			<div className=" w-full bg-gray-50 ">
+				<div className="w-[40%] m-auto">
+					{currDayHabits.map((habit, idx) => {
+						return (
+							<div key={idx} className="flex items-center justify-between">
+								<div className="border-4 grid place-items-center bg-white border-green-500  text-green-500 w-14 h-14 rounded-full " >
+									<Check className="  w-8 h-8  stroke-3" />
+								</div>
+								<div
+									className={`p-2 h-14 w-[85%] flex items-center  font-bold my-6  bg-white ${habit.color} `}
+								>
+									{habit.name}
+								</div>
+							</div>
+						);
+					})}
+				</div>
+			</div>
 		</div>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      strokeWidth: {
+        '3': '3px',
+      }
+    },
   },
   plugins: [],
 }
