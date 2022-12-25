021de042a2ee7403466b4020e7c222957f7667b3
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2022-12-25 19:29:28 +0530

    task added

--- note.js
+++ note.js
@@ -2,6 +2,8 @@
 ! fix date issues when updating the date
  the dates are not same when clicked from calendar and week calendar header
  because when clicked on calendar date and checked off the box and click the same date from week header doesnt show the checkedOff icon
+
+ problem : the thing is if i clicked on 22 and then its showing me tick on 21
 */
 
 // fix date issues when updating the date
