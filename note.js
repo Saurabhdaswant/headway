973463cae6a87d54d543a34e7591a83bd0fa4547
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2022-12-25 19:24:24 +0530

    task added

--- note.js
+++ note.js
@@ -2,5 +2,16 @@
 ! fix date issues when updating the date
  the dates are not same when clicked from calendar and week calendar header
  because when clicked on calendar date and checked off the box and click the same date from week header doesnt show the checkedOff icon
-
 */
+
+// fix date issues when updating the date
+// fix the filtering of habits timing thing
+// update the header of today component as per design
+// complete the stats and chart component from design
+// find correct way to store habits to date / date into habit object
+// refactor code using clean code practices and follow next.js course
+// use TypeScript
+// Add Animation using Framer motion
+// add signIn and sinUp functionality and store user data
+// use node js and mongodb
+// use React Querry
