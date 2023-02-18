eb0ac43707982028f9e610ebe7b0470fc80ebbe6
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2023-02-18 22:34:16 +0530

    using meaning full names

--- components/DeleteHabit.js
+++ components/DeleteHabit.js
@@ -1,28 +1,44 @@
-import React from 'react'
+import React from "react";
 
 function DeleteHabit({ habits, setHabits, habitId, toggleDeleteDialog }) {
+	
+  const handleDelete = () => {
+    const filteredHabits = habits.filter((habit, _) => habit.id !== habitId);
+    if (typeof window !== "undefined") {
+      localStorage.setItem("Habits", JSON.stringify(filteredHabits));
+    }
+    setHabits(filteredHabits);
+    toggleDeleteDialog();
+  };
 
-	const handleDelete = () => {
-		const filtered = habits.filter((h, _) => h.id !== habitId)
-		if (typeof window !== "undefined") {
-			localStorage.setItem("Habits", JSON.stringify(filtered))
-		}
-		setHabits(filtered)
-		toggleDeleteDialog()
-	}
-
-	return (
-		<div className=' fixed inset-0 z-50 flex h-full w-full items-center justify-center bg-gray-900 bg-opacity-50 '>
-			<div className='bg-white p-8 rounded-lg  ' >
-				<p className=' text-xl font-bold w-[60%] ' >Are you sure you want to delete this Habit?</p>
-				<p className='  text-gray-400 font-bold mt-2'>This action cannot be undone</p>
-				<div className='flex justify-between gap-x-4 mt-14 ' >
-					<button onClick={toggleDeleteDialog} type="submit" className=" w-full   font-semibold  bg-[#F0F0F0]  text-[#2E2E2E] px-14 rounded py-4 ">Cancel</button>
-					<button onClick={handleDelete} type="submit" className=" w-full   font-semibold  bg-[#2e2e2e] hover:bg-[#2e2e2eed] text-white px-14 rounded py-4 ">Confirm </button>
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className=" fixed inset-0 z-50 flex h-full w-full items-center justify-center bg-gray-900 bg-opacity-50 ">
+      <div className="bg-white p-8 rounded-lg  ">
+        <p className=" text-xl font-bold w-[60%] ">
+          Are you sure you want to delete this Habit?
+        </p>
+        <p className="  text-gray-400 font-bold mt-2">
+          This action cannot be undone
+        </p>
+        <div className="flex justify-between gap-x-4 mt-14 ">
+          <button
+            onClick={toggleDeleteDialog}
+            type="submit"
+            className=" w-full   font-semibold  bg-[#F0F0F0]  text-[#2E2E2E] px-14 rounded py-4 "
+          >
+            Cancel
+          </button>
+          <button
+            onClick={handleDelete}
+            type="submit"
+            className=" w-full   font-semibold  bg-[#2e2e2e] hover:bg-[#2e2e2eed] text-white px-14 rounded py-4 "
+          >
+            Confirm{" "}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default DeleteHabit
+export default DeleteHabit;
