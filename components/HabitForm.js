65dfc22ab38de1c6dd03a7583c3f8e95c3bbe13e
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2023-02-24 17:48:57 +0530

    show habit form on mobiles too

--- components/HabitForm.js
+++ components/HabitForm.js
@@ -30,7 +30,7 @@ function HabitForm({
 
   return (
     <div className=" fixed inset-0 z-40 flex h-full w-full items-center justify-center bg-gray-900 bg-opacity-50 ">
-      <div className="p-8 absolute space-y-8 top-0 right-0 w-[30%] bg-white rounded-md z-50 h-full ">
+      <div className=" p-4  lg:p-8 absolute space-y-4 lg:space-y-8 top-0 right-0 w-full lg:w-[30%] bg-white rounded-md z-50 h-full ">
         <div className="flex justify-between">
           <h1 className=" text-2xl ">{formTitle}</h1>
           <X
@@ -39,9 +39,9 @@ function HabitForm({
           />
         </div>
         <div className=" flex flex-col   h-full ">
-          <div className=" space-y-8 ">
-            <div className="flex flex-col  space-y-2">
-              <label htmlFor="habitName" className="font-semibold">
+          <div className=" space-y-4  lg:space-y-8 ">
+            <div className="flex flex-col  lg:space-y-2">
+              <label htmlFor="habitName" className="font-semibold pb-2">
                 Habit
               </label>
               <input
