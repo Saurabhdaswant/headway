11a25ff936fa9c7d345a9859972fa57e9b79542f
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2023-04-16 11:52:07 +0530

    change

--- components/DeleteHabit.js
+++ components/DeleteHabit.js
@@ -1,6 +1,5 @@
 import React, { useRef } from "react";
 import useClickOutSideToClose from "../hooks/useClickOutSideToClose";
-
 function DeleteHabit({ habits, updateHabits, habitId, toggleDeleteDialog }) {
   const handleDelete = () => {
     const filteredHabits = habits.filter((habit, _) => habit.id !== habitId);
