3a7feb6becdbf74421ba2c1f9520be02ece26554
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2023-07-15 22:44:49 +0530

    plans

--- note.js
+++ note.js
@@ -15,7 +15,21 @@
 // find correct way to store habits to date / date into habit object
 // learn typescript for react && next.js and refactor the code
 
+// how can i take this thing to next level ?
 
+// UI enhancements: Continually improve your UI based on user feedback and the latest design trends.
+// For instance, you could add a dark mode, create a more intuitive onboarding process, or provide more customization options for the users.
+
+// Performance: Ensure the app loads quickly, runs smoothly, and is optimized for different devices. React's virtual DOM
+//  provides opportunities for fine-tuning performance that can make a significant difference.
+
+// Accessibility: Make sure your app is accessible to users with disabilities. This can involve providing alternative text for
+// images, ensuring good contrast between text and background, and making sure the app can be fully used with a keyboard.
+
+// Social Features: Allowing users to share their progress, join groups, or compete in challenges could make the app more engaging.
+
+// Notifications and Reminders: Users could benefit from customizable reminders about their habits. You could also use notifications
+// to celebrate milestones or suggest new habits based on their progress.
 
 //? head lines :
 // "Empower yourself to achieve greatness with Headway"
@@ -29,15 +43,8 @@
 // "Track your progress and make headway towards your goals with Headway"
 // "Unlock the power of self-improvement and make headway with Headway
 
-
-
-
-    // const byDate = habit => isBefore(selectedDay, new Date(habit.createdDate))
-    // const byDay = habit => !habit.repeatHabitDays.includes(selectedDay)
-
-
-
-
+// const byDate = habit => isBefore(selectedDay, new Date(habit.createdDate))
+// const byDay = habit => !habit.repeatHabitDays.includes(selectedDay)
 
 //! main stuff
 // app
