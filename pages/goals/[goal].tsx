23fdcf1c3ea783b0c8f3280ce8f0eabc4a75bc97
saurabh <77434364+Saurabhdaswant@users.noreply.github.com>
2024-06-25 09:29:18 +0530

    goal detail page design - wip

--- pages/goals/[goal].tsx
+++ pages/goals/[goal].tsx
@@ -1,13 +1,42 @@
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { API_ENDPOINTS } from "../../constants";
+import { format, startOfToday, formatDistance } from "date-fns";
+import { Calendar, Plus } from "react-feather";
+import { AnimatePresence } from "framer-motion";
+import HabitForm from "../../components/HabitForm";
+import { weekDays } from "../../components/constants";
+import { Habits as HabitsRenderer } from "../../components/Habits";
 
 export default function Goal() {
   const router = useRouter();
   const { goal: id } = router.query;
   const [goal, setGoal] = useState<any>();
+  const today = startOfToday();
+  const [error, setError] = useState(false);
+  const [token, setToken] = useState(null);
+  const [showHabitForm, setShowHabitForm] = useState(false);
 
-  console.log(id, "right ?");
+  const newHabit = {
+    name: "",
+    getDoneIn: "anytime",
+    color: "",
+    completedOnDates: [],
+    createdDate: today,
+    repeatHabitDays: weekDays,
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const token = window.localStorage.getItem("authToken");
+
+      if (!token) {
+        window.location.href = "/login";
+      }
+
+      setToken(token);
+    }
+  }, []);
 
   useEffect(() => {
     const token = localStorage && localStorage?.getItem("authToken");
@@ -29,7 +58,110 @@ export default function Goal() {
         getGoals();
       }
     }
-  }, []);
+  }, [id]);
 
-  return <div>actual goal name : {goal?.name} </div>;
+  const handleCreateHabit = async (habit) => {
+    if (habit.name.trim().length === 0) {
+      setError(true);
+      return;
+    } else {
+      setError(false);
+
+      if (habit.getDoneIn === "") {
+        habit.getDoneIn = "anytime";
+      }
+
+      const res = await fetch(
+        `${API_ENDPOINTS.BASE_URL}/goals/addHabit/${goal._id}`,
+        {
+          method: "PUT",
+          body: JSON.stringify({ habit }),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const data = await res.json();
+
+      const newHabits = [...(goal.habits || []), data.habit];
+      setGoal({ ...goal, habits: newHabits });
+      setShowHabitForm(false);
+    }
+  };
+
+  return (
+    <div className=" mx-auto max-w-[600px] pt-10">
+      <div className="z-10">
+        <p className=" font-bold text-4xl  text-[#272727] capitalize   ">
+          {" "}
+          {goal?.name}
+        </p>
+        <p className=" font-normal text-gray-600  mt-6    ">
+          {goal?.description}
+        </p>
+      </div>{" "}
+      <div className="flex mt-8 items-center gap-1">
+        <Calendar strokeWidth="2" size="20" />
+        <p
+          className={`text-sm font-medium    capitalize    rounded-full    text-gray-600 `}
+        >
+          started on
+        </p>
+
+        <p
+          className={`text-sm py-1    capitalize  font-bold    rounded-full    text-gray-900 `}
+        >
+          {goal ? format(new Date(goal?.createdDate), "MM/dd/yyyy") : null}
+        </p>
+      </div>
+      <div className="flex items-center gap-4 mt-10">
+        <div className="p-6 bg-gray-100 rounded-lg">
+          <p className={`    capitalize    rounded-full    text-gray-600 `}>
+            deadline date
+          </p>
+          <p
+            className={` text-4xl font-black py-1    capitalize     rounded-full    text-gray-900 `}
+          >
+            {goal ? format(new Date(goal?.deadlineDate), "MM/dd/yyyy") : null}
+          </p>
+        </div>
+        <div className="p-6 bg-gray-100 rounded-lg">
+          <p className={`    capitalize    rounded-full    text-gray-600 `}>
+            time left
+          </p>
+          <p
+            className={` text-4xl font-black py-1    capitalize    rounded-full    text-gray-900 `}
+          >
+            {goal ? formatDistance(today, new Date(goal?.deadlineDate)) : null}
+          </p>
+        </div>
+      </div>
+      <div
+        onClick={() => {
+          setShowHabitForm(true);
+        }}
+        className="flex mt-8 items-center gap-1"
+      >
+        <Plus strokeWidth="2" size="20" />
+        <p
+          className={`text-sm font-medium    capitalize    rounded-full    text-gray-600 `}
+        >
+          Add Habits
+        </p>
+      </div>
+      <HabitsRenderer habits={goal?.habits} selectedDay={today} />
+      <AnimatePresence>
+        {showHabitForm ? (
+          <HabitForm
+            formTitle="Add New Habit"
+            habit={newHabit}
+            setShowHabitForm={setShowHabitForm}
+            handleSubmit={handleCreateHabit}
+            error={error}
+          />
+        ) : null}
+      </AnimatePresence>
+    </div>
+  );
 }
