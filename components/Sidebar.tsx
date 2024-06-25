7bf56deef131bad51ab52267aacacbeebccb5a68
saurabh <77434364+Saurabhdaswant@users.noreply.github.com>
2024-06-25 16:56:17 +0530

    sidebar and more cool stuff

---
+++ components/Sidebar.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { ChartBarIcon } from "@heroicons/react/outline";
+import { CheckCircle, Headphones, Target, Users, Zap } from "react-feather";
+import { useRouter } from "next/router";
+
+const sidebarItems = [
+  { name: "Dashboard", icon: ChartBarIcon, path: "/" },
+  { name: "Goals", icon: Target, path: "/goals" },
+  { name: "Habits", icon: CheckCircle, path: "/habits" },
+  { name: "Tribe", icon: Users, path: "/tribe" },
+  { name: "Workouts", icon: Zap, path: "/workouts" },
+  { name: "Meditations", icon: Headphones, path: "/meditations" },
+];
+
+function Sidebar() {
+  const router = useRouter();
+
+  const { pathname } = router;
+
+  return (
+    <div className="z-50 w-[20%] bg-gray-50">
+      <div className="flex flex-col mt-10 justify-between h-[90%]">
+        <ul className="px-4">
+          {sidebarItems.map((item) => {
+            const Icon = item.icon;
+            const isActive = pathname === item.path;
+            return (
+              <li
+                key={item.name}
+                onClick={() => router.push(item.path)}
+                className={`py-4 px-4 flex items-center gap-2 cursor-pointer rounded-xl font-medium ${
+                  isActive ? "bg-[#2e2e2e] text-white" : ""
+                }`}
+              >
+                <Icon className="w-5 h-5" /> {item.name}
+              </li>
+            );
+          })}
+        </ul>
+        <div className="flex items-center justify-center gap-2 border-t py-4 border-gray-200">
+          <div className="w-12 h-12 bg-slate-300 rounded-full"></div>
+          <p className="font-medium">Saurabh</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
--- pages/goals/[goal].tsx
+++ pages/goals/[goal].tsx
@@ -7,6 +7,7 @@ import { AnimatePresence } from "framer-motion";
 import HabitForm from "../../components/HabitForm";
 import { weekDays } from "../../components/constants";
 import { Habits as HabitsRenderer } from "../../components/Habits";
+import Layout from "../../components/Layout";
 
 export default function Goal() {
   const router = useRouter();
@@ -91,77 +92,81 @@ export default function Goal() {
   };
 
   return (
-    <div className=" mx-auto max-w-[600px] pt-10">
-      <div className="z-10">
-        <p className=" font-bold text-4xl  text-[#272727] capitalize   ">
-          {" "}
-          {goal?.name}
-        </p>
-        <p className=" font-normal text-gray-600  mt-6    ">
-          {goal?.description}
-        </p>
-      </div>{" "}
-      <div className="flex mt-8 items-center gap-1">
-        <Calendar strokeWidth="2" size="20" />
-        <p
-          className={`text-sm font-medium    capitalize    rounded-full    text-gray-600 `}
-        >
-          started on
-        </p>
+    <Layout>
+      <div className=" mx-auto  pt-10">
+        <div className="z-10">
+          <p className=" font-bold text-4xl  text-[#272727] capitalize   ">
+            {" "}
+            {goal?.name}
+          </p>
+          <p className=" font-normal text-gray-600  mt-6    ">
+            {goal?.description}
+          </p>
+        </div>{" "}
+        <div className="flex mt-8 items-center gap-1">
+          <Calendar strokeWidth="2" size="20" />
+          <p
+            className={`text-sm font-medium    capitalize    rounded-full    text-gray-600 `}
+          >
+            started on
+          </p>
 
-        <p
-          className={`text-sm py-1    capitalize  font-bold    rounded-full    text-gray-900 `}
-        >
-          {goal ? format(new Date(goal?.createdDate), "MM/dd/yyyy") : null}
-        </p>
-      </div>
-      <div className="flex items-center gap-4 mt-10">
-        <div className="p-6 bg-gray-100 rounded-lg">
-          <p className={`    capitalize    rounded-full    text-gray-600 `}>
-            deadline date
-          </p>
           <p
-            className={` text-4xl font-black py-1    capitalize     rounded-full    text-gray-900 `}
+            className={`text-sm py-1    capitalize  font-bold    rounded-full    text-gray-900 `}
           >
-            {goal ? format(new Date(goal?.deadlineDate), "MM/dd/yyyy") : null}
+            {goal ? format(new Date(goal?.createdDate), "MM/dd/yyyy") : null}
           </p>
         </div>
-        <div className="p-6 bg-gray-100 rounded-lg">
-          <p className={`    capitalize    rounded-full    text-gray-600 `}>
-            time left
-          </p>
+        <div className="flex items-center gap-4 mt-10">
+          <div className="p-6 bg-gray-100 rounded-lg">
+            <p className={`    capitalize    rounded-full    text-gray-600 `}>
+              deadline date
+            </p>
+            <p
+              className={` text-4xl font-black py-1    capitalize     rounded-full    text-gray-900 `}
+            >
+              {goal ? format(new Date(goal?.deadlineDate), "MM/dd/yyyy") : null}
+            </p>
+          </div>
+          <div className="p-6 bg-gray-100 rounded-lg">
+            <p className={`    capitalize    rounded-full    text-gray-600 `}>
+              time left
+            </p>
+            <p
+              className={` text-4xl font-black py-1    capitalize    rounded-full    text-gray-900 `}
+            >
+              {goal
+                ? formatDistance(today, new Date(goal?.deadlineDate))
+                : null}
+            </p>
+          </div>
+        </div>
+        <div
+          onClick={() => {
+            setShowHabitForm(true);
+          }}
+          className="flex mt-8 items-center gap-1"
+        >
+          <Plus strokeWidth="2" size="20" />
           <p
-            className={` text-4xl font-black py-1    capitalize    rounded-full    text-gray-900 `}
+            className={`text-sm font-medium    capitalize    rounded-full    text-gray-600 `}
           >
-            {goal ? formatDistance(today, new Date(goal?.deadlineDate)) : null}
+            Add Habits
           </p>
         </div>
+        <HabitsRenderer habits={goal?.habits} selectedDay={today} />
+        <AnimatePresence>
+          {showHabitForm ? (
+            <HabitForm
+              formTitle="Add New Habit"
+              habit={newHabit}
+              setShowHabitForm={setShowHabitForm}
+              handleSubmit={handleCreateHabit}
+              error={error}
+            />
+          ) : null}
+        </AnimatePresence>
       </div>
-      <div
-        onClick={() => {
-          setShowHabitForm(true);
-        }}
-        className="flex mt-8 items-center gap-1"
-      >
-        <Plus strokeWidth="2" size="20" />
-        <p
-          className={`text-sm font-medium    capitalize    rounded-full    text-gray-600 `}
-        >
-          Add Habits
-        </p>
-      </div>
-      <HabitsRenderer habits={goal?.habits} selectedDay={today} />
-      <AnimatePresence>
-        {showHabitForm ? (
-          <HabitForm
-            formTitle="Add New Habit"
-            habit={newHabit}
-            setShowHabitForm={setShowHabitForm}
-            handleSubmit={handleCreateHabit}
-            error={error}
-          />
-        ) : null}
-      </AnimatePresence>
-    </div>
+    </Layout>
   );
 }
